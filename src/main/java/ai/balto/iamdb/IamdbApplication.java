9775b70fa48273c9f42a622d900014922926cd1b
Tai <imadeanomelet@gmail.com>
2021-07-27 10:33:07 -0500

    Initial commit w/ Springboot setup.

---
+++ src/main/java/ai/balto/iamdb/IamdbApplication.java
@@ -0,0 +1,13 @@
+package ai.balto.iamdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/balto/iamdb/IamdbApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.balto.iamdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
